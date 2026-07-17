91750463e8d044c6947ee5e403b8a061e51f3e33
TOLIHO <cumbercube3d@gmail.com>
2026-07-17 17:02:23 +0100

    Create portfolio-projects.js

---
+++ js/portfolio-projects.js
@@ -0,0 +1,85 @@
+const projects = [
+
+{
+    title: "Dark Angels Bolter",
+    date: "05-04-2025",
+    link: "projects/project1.html",
+    image: "images/project1.jpg",
+    description: "First full model using the Blender pipeline.",
+    tags: [
+        "Blender",
+        "Substance Painter",
+        "Prop Model",
+        "Weapon Model"
+    ]
+},
+
+{
+    title: "Red Menace: Beyond the Threshold",
+    date: "18-06-2025",
+    link: "projects/project2.html",
+    image: "images/project2.jpg",
+    description: "A detailed environment showcasing VFX, texturing and modular set creation.",
+    tags: [
+        "Environment Art",
+        "Substance Painter",
+        "Substance Designer",
+        "MA Project",
+        "Report",
+        "UE5",
+        "3DS Max",
+        "Niagara"
+    ]
+},
+
+{
+    title: "Witchhunter's Ordained Rifle",
+    date: "12-07-2025",
+    link: "projects/project3.html",
+    image: "images/project3.jpg",
+    description: "A weapon prop created to further develop my skills with PBR materials and texturing.",
+    tags: [
+        "3DS Max",
+        "Marmoset Toolbag",
+        "Substance Painter",
+        "Substance Designer",
+        "MA Project",
+        "Prop Model",
+        "Weapon Model"
+    ]
+},
+
+{
+    title: "Vault 101 - Reimagined",
+    date: "22-07-2024",
+    link: "projects/project4.html",
+    image: "images/project4.jpg",
+    description: "A modular environment recreating the opening scenes from Fallout 3.",
+    tags: [
+        "Environment",
+        "Quixel",
+        "Substance Painter",
+        "UE4",
+        "3DS Max",
+        "Niagara",
+        "BA Project"
+    ]
+},
+
+{
+    title: "Arch Viz - Dining Room and Social Area",
+    date: "10-05-2024",
+    link: "projects/project5.html",
+    image: "images/project5.jpg",
+    description: "An early BA architectural visualisation project.",
+    tags: [
+        "Environment",
+        "Arch Viz",
+        "UE4",
+        "Substance Painter",
+        "Substance Designer",
+        "BA Project"
+    ]
+}
+
+];
